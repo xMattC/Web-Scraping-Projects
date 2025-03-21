78cb1e31a0eb15fbd31b63da3dd71175e8b7aef6
Matt Corcoran <mkcorcoran2015@gmail.com>
2025-03-21 17:10:14 +0100

    re-formmaing of upsplash_images.py

--- scrapers/upsplash_images.py
+++ scrapers/upsplash_images.py
@@ -10,12 +10,6 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 
 def scrape_up_splash(terms: list[str]):
     """Scrapes Unsplash for images related to the given search terms and saves them.
-
-    Args:
-        terms (list[str]): List of search terms for which images should be fetched.
-
-    Raises:
-        Exception: If no search term is provided.
     """
     if not terms or len(terms) == 0:
         raise Exception("No search term provided")
@@ -42,25 +36,12 @@ def get_img_tags_for(term: str) -> list:
 
 def img_filter_out(url: str, keywords: list[str]) -> bool:
     """Checks if an image URL contains any unwanted keywords and filters it out.
-
-    Args:
-        url (str): Image URL to check.
-        keywords (list[str]): List of keywords to filter out (e.g., 'plus', 'profile', 'premium').
-
-    Returns:
-        bool: True if the URL does not contain any filtered keywords, otherwise False.
     """
     return not any(x in url for x in keywords)
 
 
 def get_high_res_img_url(img_node) -> str | None:
     """Extracts the highest-resolution image URL from the srcset attribute.
-
-    Args:
-        img_node: HTML node containing image information.
-
-    Returns:
-        str | None: High-resolution image URL or None if filtering removes all options.
     """
     srcset = img_node.attrs["srcset"]  # Get srcset attribute containing multiple image URLs
     srcset_list = srcset.split(", ")  # Split into individual image entries
@@ -76,11 +57,6 @@ def get_high_res_img_url(img_node) -> str | None:
 
 def save_images(img_urls: list[str], term: str, tag: str = ""):
     """Downloads images from given URLs and saves them to a directory.
-
-    Args:
-        img_urls (list[str]): List of image URLs to download.
-        term (str): Search term used to name the output directory.
-        tag (str, optional): Optional tag used in saved filenames. Defaults to "".
     """
     for url in img_urls:
         resp = get(url)  # Send GET request to download image

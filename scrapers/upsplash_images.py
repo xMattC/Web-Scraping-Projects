443c1b3cacfe9a7a22a90cda0d2acb4f939d04ab
Matt Corcoran <mkcorcoran2015@gmail.com>
2025-03-22 11:52:02 +0100

    trying to scrape up-splash using api intercept method.

--- scrapers/upsplash_images.py
+++ scrapers/upsplash_images.py
@@ -9,21 +9,27 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 
 
 def scrape_up_splash(terms: list[str]):
-    """Scrapes Unsplash for images related to the given search terms and saves them.
+    """ Scrapes Unsplash for images related to the given search terms and saves them.
     """
     if not terms or len(terms) == 0:
-        raise Exception("No search term provided")
+        raise Exception("No search terms provided")
 
-    for term in terms:
-        img_nodes = get_img_tags_for(term)  # Extract image nodes from Unsplash search results
-        all_img_urls = [get_high_res_img_url(i) for i in img_nodes]  # Extract high-resolution image URLs
-        img_urls = [u for u in all_img_urls if u]  # Filter out None values
+    for i, term in enumerate(terms):
 
-        save_images(img_urls, term, term)  # Download and save images
+        if type(term) == str:
+
+            img_nodes = get_img_tags_for(term)  # Extract image nodes from Unsplash search results
+            all_img_urls = [get_high_res_img_url(i) for i in img_nodes]  # Extract high-resolution image URLs
+            img_urls = [u for u in all_img_urls if u]  # Filter out None values
+
+            save_images(img_urls, term, term)  # Download and save images
+
+        else:
+            logging.warning(f"Input list element {i} - ({term}) is not of type string. Ignoring element")
 
 
 def get_img_tags_for(term: str) -> list:
-    """Fetches image elements from Unsplash search results.
+    """ Fetches image elements from Unsplash search results.
     """
     url = f"https://unsplash.com/s/photos/{term}"
     html = extract_full_body_html(url)
@@ -35,19 +41,19 @@ def get_img_tags_for(term: str) -> list:
 
 
 def img_filter_out(url: str, keywords: list[str]) -> bool:
-    """Checks if an image URL contains any unwanted keywords and filters it out.
+    """ Checks if an image URL contains any unwanted keywords and filters it out.
     """
     return not any(x in url for x in keywords)
 
 
 def get_high_res_img_url(img_node) -> str | None:
-    """Extracts the highest-resolution image URL from the srcset attribute.
+    """ Extracts the highest-resolution image URL from the srcset attribute.
     """
-    srcset = img_node.attrs["srcset"]  # Get srcset attribute containing multiple image URLs
-    srcset_list = srcset.split(", ")  # Split into individual image entries
+    src_set = img_node.attrs["srcset"]  # Get "srcset" attribute containing multiple image URLs
+    src_set_list = src_set.split(", ")  # Split into individual image entries
 
     # Extract URL with the highest resolution, filtering out unwanted keywords
-    url_res = [src.split(" ") for src in srcset_list if img_filter_out(src, ['plus', 'profile', 'premium'])]
+    url_res = [src.split(" ") for src in src_set_list if img_filter_out(src, ['plus', 'profile', 'premium'])]
 
     if not url_res:
         return None  # Return None if no valid image URLs remain
@@ -56,7 +62,7 @@ def get_high_res_img_url(img_node) -> str | None:
 
 
 def save_images(img_urls: list[str], term: str, tag: str = ""):
-    """Downloads images from given URLs and saves them to a directory.
+    """ Downloads images from given URLs and saves them to a directory.
     """
     for url in img_urls:
         resp = get(url)  # Send GET request to download image
@@ -74,8 +80,21 @@ def save_images(img_urls: list[str], term: str, tag: str = ""):
             logging.info(f"Saved {file_name}, with size {round(len(resp.content) / 1024 / 1024, 2)} MB.")
 
 
+def try_with_api_intercept(query: str, per_page='10'):
+    """
+    """
+    url = f"https://unsplash.com/napi/search/photos?page=1&per_page={per_page}&query={query}"
+    html = extract_full_body_html(url)
+    resp = get(html)
+
+    if resp.status_code == 200:
+        return resp.json()
+
+
 if __name__ == '__main__':
     # Run the scraper with a predefined list of search terms
-    scrape_up_splash(
-        ['lions', 'tigers', 'wolves', 'foxes', 'bears', 'rabbits', 'horses', 'elephants', 'giraffes', 'zebras']
-    )
+    # scrape_up_splash(
+    #     ['tigers', 'wolves', 'foxes', 'bears', 'rabbits']
+    # )
+
+    print(try_with_api_intercept())

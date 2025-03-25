3a52c8dbf384b398114b600f0a4882d40b47ea3c
Matt Corcoran <mkcorcoran2015@gmail.com>
2025-03-25 17:32:41 +0100

    reformat

--- scrapers/countries_gdp/countries_gdp/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class CountryGdpItem(scrapy.Item):
-    # define the fields for your item here like:
-    country_name = scrapy.Field()
-    gdp = scrapy.Field()
-    year = scrapy.Field()
-

1e6628de602fd82644fa72c0a0b86d169641e6c9
Matt Corcoran <mkcorcoran2015@gmail.com>
2025-03-27 11:06:02 +0100

    Set pipline execution order

--- scrapers/countries_gdp/countries_gdp/middlewares.py
+++ scrapers/countries_gdp/countries_gdp/middlewares.py
@@ -1,8 +1,3 @@
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 from scrapy import signals
 
 # useful for handling different item types with a single interface
@@ -22,10 +17,6 @@ class CountriesGdpSpiderMiddleware:
         return s
 
     def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
         return None
 
     def process_spider_output(self, response, result, spider):
@@ -57,9 +48,6 @@ class CountriesGdpSpiderMiddleware:
 
 
 class CountriesGdpDownloaderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
 
     @classmethod
     def from_crawler(cls, crawler):

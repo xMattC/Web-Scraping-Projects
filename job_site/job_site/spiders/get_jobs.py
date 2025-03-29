44a6f1273905821e0634c47c5b897ba0e4b3a25a
Matt Corcoran <mkcorcoran2015@gmail.com>
2025-03-29 11:30:41 +0100

    update to JavaScript methods

--- job_site/job_site/spiders/get_jobs.py
+++ job_site/job_site/spiders/get_jobs.py
@@ -11,18 +11,22 @@ class GetJobsSpider(scrapy.Spider):
     allowed_domains = ["www.workingnomads.com"]
     start_urls = ["https://www.workingnomads.com/jobs"]
 
-    def __init__(self, n_listings=150, **kwargs):
+    def __init__(self, n_listings=500, **kwargs):
         super().__init__(**kwargs)
         self.required_jobs = int(n_listings)
         self.js_code = self.load_js_code(3000)
 
     def load_js_code(self, click_timeout):
+
+        load_pages = math.ceil(self.required_jobs / 100) - 1
         file_path = Path.joinpath(Path.cwd(), "job_site/spiders/page_method.js")
+
         with open(file_path, "r") as js_file:
             js_code = js_file.read()
-            load_pages = math.ceil((self.required_jobs / 100)) - 1
-            js_code = js_code.replace("LOAD_PAGES", str(load_pages))
-            js_code = js_code.replace("CLICK_TIMEOUT", str(click_timeout))
+
+        js_code = js_code.replace("LOAD_PAGES", str(load_pages))
+        js_code = js_code.replace("CLICK_TIMEOUT", str(click_timeout))
+
         return js_code
 
     def start_requests(self):
@@ -34,12 +38,37 @@ class GetJobsSpider(scrapy.Spider):
                     PageMethod("wait_for_selector", 'div.jobs-list div.job-wrapper'),
                     PageMethod("wait_for_selector", "#accept-btn"),
                     PageMethod("click", "#accept-btn"),
+                    # PageMethod("evaluate", f"{self.js_code}"),
                     PageMethod("evaluate", self.js_code),
+
+                    # PageMethod("evaluate",
+                    #
+                    #            f"""
+                    #                 let count = 0;
+                    #                 const load_pages = {5};  // Injected Python variable
+                    #                 async function clickLoadMore() {{
+                    #                     while (count < load_pages) {{
+                    #                         const button = document.querySelector('div.show-more');
+                    #
+                    #                         if (button) {{
+                    #                             button.scrollIntoView({{ behavior: 'smooth', block: 'center' }});
+                    #                             button.click();
+                    #                             count++;
+                    #                             console.log("Clicked, count:", count);  // Debug log
+                    #                             await new Promise(resolve => setTimeout(resolve, 5000));
+                    #
+                    #                         }} else {{
+                    #                             break;
+                    #                         }}
+                    #                     }}
+                    #                 }}
+                    #                 clickLoadMore();
+                    #             """),
                 ]
             )
         )
 
-    def parse(self, response):
+    async def parse(self, response):
         for job in response.css('div.jobs-list div.job-wrapper'):
             item = ItemLoader(item=JobContainerItem(), selector=job)
             item.add_css("job_name", 'a.open-button.ng-binding::text')
@@ -51,52 +80,6 @@ class GetJobsSpider(scrapy.Spider):
             item.add_css("tags", 'div.box i.fa-tags + a::text')
             yield item.load_item()
 
-            #  Before item processing:
             # yield {
             #     "job_name": job.css("a.open-button.ng-binding::text").get(),
-            #     # "job_link": job.css("a.open-button.ng-binding::attr(href)").get(),
-            #     # "ng_href": job.css("a.open-button.ng-binding::attr(ng-href)").get(),
-            #     # "company_name": job.css("div.company a::text").get(),
-            #     # "job_location": job.css("div.box i.fa-map-marker + span::text").get(),
-            #     # "work_type": job.css("div.box i.fa-clock-o + span::text").get(),
-            #     # "tags": job.css("div.box i.fa-tags + a::text").getall(),
-            #     # "salary": job.css("div.box i.fa-money + span::text").get() or job.css(
-            #     #     "div.box.ng-hide i.fa-money + span::text").get(),
             # }
-            #
-            # item = ItemLoader(item=JobContainerItem(), selector=job)
-            # item.add_css("job_name", 'a.open-button.ng-binding::text')
-            # yield item.load_item
-
-            # This yielded nothing:
-            # async def parse(self, response):
-            #     for job in response.css('div.jobs-list div.job-wrapper'):
-            #         item = ItemLoader(item=JobContainerItem(), selector=job)
-            #         item.add_css("job_name", 'a.open-button.ng-binding::text')
-            #         yield item.load_item
-            #
-            #     howver this worked:
-            #     yield {
-            #         item = ItemLoader(item=JobContainerItem(), selector=job)
-            #         item.add_css("job_name", 'a.open-button.ng-binding::text')
-            #     yield item.load_item
-            #
-            #     my item.py looks like
-            #     import scrapy
-            #     from itemloaders.processors import TakeFirst, MapCompose, Join
-            #     from w3lib.html import remove_tags
-            #
-            #     def remove_newlines(value):
-            #         return value.replace("\n", "").strip()  # Remove newlines and extra spaces
-            #
-            #     class JobContainerItem(scrapy.Item):
-            #         # Country name field with text cleaning and extraction
-            #         job_name = scrapy.Field(
-            #             input_processor=MapCompose(remove_newlines),  # Remove HTML tags and extra spaces
-            #             output_processor=Join()  # Take the first extracted value
-            #         )
-            #
-            #     and these are my imports:
-            #
-            #     from scrapy.loader import ItemLoader
-            #     from ..items import JobContainerItem
--- job_site/job_site/spiders/page_method.js
+++ job_site/job_site/spiders/page_method.js
@@ -1,20 +1,21 @@
-let count = 0; // Counter to track the number of clicks
-const load_pages = LOAD_PAGES; // Injected variable: number of pages to load
-const click_timeout = CLICK_TIMEOUT; // Injected variable: timeout between clicks
+let count = 0;  // Initialize a counter to track the number of "Load More" clicks
+const load_pages = LOAD_PAGES;  // Placeholder for injected Python variable (number of pages to load)
+const click_timeout = CLICK_TIMEOUT;  // Placeholder for injected Python variable (timeout between clicks)
 
 async function clickLoadMore() {
-    while (count < load_pages) { // Keep clicking until the target number of pages is loaded
-        const button = document.querySelector('div.show-more'); // Find the "Load More" button
-        
-        if (button) { // If the button is found, proceed with clicking
-            button.scrollIntoView({ behavior: 'smooth', block: 'center' }); // Smoothly scroll the button into view
-            button.click(); // Simulate the click on the button
-            count++; // Increment the count after each click
-            console.log("Clicked, count:", count); // Debug log to show progress
+    while (count < load_pages) {  // Continue clicking while the count is less than the number of pages
+        const button = document.querySelector('div.show-more');  // Find the "Load More" button using its CSS selector
 
-            await new Promise(resolve => setTimeout(resolve, click_timeout)); // Wait for the timeout before clicking again
+        if (button) {  // If the "Load More" button exists on the page
+            button.scrollIntoView({ behavior: 'smooth', block: 'center' });  // Scroll the button into view smoothly
+            button.click();  // Click the "Load More" button
+            count++;  // Increment the counter after each click
+            console.log("Clicked, count:", count);  // Debug log to track how many times the button has been clicked
+            await new Promise(resolve => setTimeout(resolve, click_timeout));  // Wait for the specified timeout before the next click
         } else {
-            break; // If no button is found, stop the loop
+            break;  // If no "Load More" button is found, stop the loop
         }
     }
 }
+
+clickLoadMore();  // Call the function to start the "Load More" clicking process
